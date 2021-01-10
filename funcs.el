41e8a0acf0aa28414065ab05ec2d9861a60fa1c8
Zhe Li <linuxcity.jn@gmail.com>
2021-01-10 23:41:03 +0100

    Rename org-gcal-refresh-token to org-gcal--refresh-token due to change in org-gcal

--- funcs.el
+++ funcs.el
@@ -13,7 +13,7 @@
   (defun google-calendar/org-gcal-update ()
     "Refresh OAuth token, fetch and sync calendar"
     (interactive)
-    (org-gcal-refresh-token)
+    (org-gcal--refresh-token)
     (org-gcal-fetch))
 
   (defun google-calendar/sync-cal-after-capture ()
@@ -24,7 +24,7 @@ The function can be run automatically with the 'org-capture-after-finalize-hook'
                (cal-file-exists (and (mapcar 'f-file? cal-files)))
                (capture-target-isfile (eq (car (org-capture-get :target)) 'file))
                (capture-target-is-cal-file (member capture-target cal-files)))
-      (org-gcal-refresh-token)
+      (org-gcal--refresh-token)
       (org-gcal-post-at-point))))
 
 (when (configuration-layer/package-usedp 'calfw)
